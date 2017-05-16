b6b64e13f36f038b403d59e7d5d1e3a13b3b99ea
Tomi Engel <tomi@objectfarm.org>
2017-05-16 11:13:57 +0200

    Provide some more insight into the current build environment.

--- build.sh
+++ build.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
+
+echo "Builing with environment:"
+echo "  GOROOT: `go env GOROOT`"
+echo "  GOPATH: `go env GOPATH`"
+echo "    GOOS: `go env GOOS`"
+echo "  GOARCH: `go env GOARCH`"
+
+GOPATH = `go env GOPATH`
+#GOOS=solaris
+#GOARCH=amd64
+#export GOOS GOARCH
+
 mkdir $GOPATH/bin/solaris
-GOOS=solaris
-export GOOS
-go get golang.org/x/sys/unix
-GOARCH=amd64 go build -ldflags '-linkmode internal' -o $GOPATH/bin/solaris/smartos_exporter github.com/ingrians/smartos_exporter
+#go get golang.org/x/sys/unix
+#go build -ldflags '-linkmode internal' -o $GOPATH/bin/solaris/smartos_exporter github.com/tomi-engel/smartos_exporter
+
+go build -v
